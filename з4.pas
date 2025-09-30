3642d0f30a37a1f2e595329fbd9607ef17acbae2
kolyamba1496 <kolyamba148896@gmail.com>
2025-09-30 21:20:36 +0300

    Add files via upload

---
+++ з4.pas
@@ -0,0 +1,42 @@
+﻿program z4;
+var year,minYear, maxYear, i: integer;
+  kolvoos, // количество осадков для нынешного года
+  minos,     // минимальное колво осадков 
+  maxos: real; // Максимальное колво осадков 
+begin
+  writeln('Введите количество осадков в мм:');
+
+  write('2015 год: ');
+  readln(kolvoos);
+  
+  minos := kolvoos; 
+  maxos := kolvoos; 
+  minYear := 2011;  
+  maxYear := 2011;  
+
+  for i := 1 to 10 do
+  begin
+    year := 2015 + i; 
+    write(year, ' год: ');
+
+    readln(kolvoos);
+    
+    if kolvoos < minos then
+    begin
+      minos := kolvoos; // обновляю значение минимума
+      minYear := year;
+    end;
+    
+    if kolvoos > maxos then
+    begin
+      maxos:= kolvoos; // обновляю значение максимума
+      minYear := year;
+    end;
+  end;
+  
+  writeln;
+
+  writeln('самый засушливый год: ', minYear, ' (', minos:0:1, ' мм)');
+  // Вывод самого влажного года
+  writeln('самый влажный год: ', maxYear, ' (', maxos:0:1, ' мм)');
+end.
---
+++ з4.py
@@ -0,0 +1,28 @@
+print('введите количество осадков в мм:')
+
+print('2015 год: ')
+kolvoos = float(input())
+
+minos = kolvoos
+maxos = kolvoos
+minYear = 2011
+maxYear = 2011
+
+for i in range(1, 11):
+    year = 2015 + i
+    print(year, ' год: ')
+
+    kolvoos = float(input())
+
+    if kolvoos < minos:
+        minos = kolvoos
+        minYear = year
+
+    if kolvoos > maxos:
+        maxos = kolvoos
+        minYear = year
+
+print()
+
+print('самый засушливый год: ', minYear, ' (', format(minos, '.1f'), ' мм)')
+print('самый влажный год: ', maxYear, ' (', format(maxos, '.1f'), ' мм)')
