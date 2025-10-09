0f3f8a704dc35c023a0dc1f01a7d8fc57791fb60
kolyamba1496 <kolyamba148896@gmail.com>
2025-10-09 11:02:37 +0300

    з3.py

--- з3.py
+++ з3.py
@@ -1,13 +1,16 @@
-A1, A2, A3, A4 = map(float, input().split())
-n = int(input())
+A1, A2, A3, A4 = map(float, input("Введите A1 A2 A3 A4: ").split())
+n = int(input("Введите количество точек: "))
 c1 = c2 = c3 = 0
 i = 1
-
+print("Введите координаты точек:")
 while True:
-    x = float(input())
+    x = float(input(f"Точка {i}: "))
     if A1 <= x <= A2: c1 += 1
     if A2 <= x <= A3: c2 += 1
     if A3 <= x <= A4: c3 += 1
     i += 1
-    if i > n: break  
-print(c1, c2, c3)
+    if i > n: break
+print("Результат:")
+print(f"Отрезок 1: {c1} точек")
+print(f"Отрезок 2: {c2} точек")
+print(f"Отрезок 3: {c3} точек")
